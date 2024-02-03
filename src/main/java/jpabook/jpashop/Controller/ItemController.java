83695816134f5ac4d8fb8ecba8918b4aebeacb78
saebyeolKim <toquf2611@naver.com>
2024-02-03 17:14:35 +0900

    feat: 상품 등록

---
+++ src/main/java/jpabook/jpashop/Controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.Controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        //createBook 같은 비즈니스 로직을 만들어서 setter 를 제거해야 함
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
