885ea4d1c315219b4b43880092cd116d0aade83c
saebyeolKim <toquf2611@naver.com>
2024-02-03 16:56:42 +0900

    feat: 회원 등록

---
+++ src/main/java/jpabook/jpashop/Controller/MemberForm.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.Controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+    private String city;
+    private String zipcode;
+    private String street;
+}
