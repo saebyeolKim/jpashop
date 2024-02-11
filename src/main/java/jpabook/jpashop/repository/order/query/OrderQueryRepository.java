e8e6e7055e80b134c0db3b1704cdb2b84b494e52
saebyeolKim <toquf2611@naver.com>
2024-02-11 14:36:56 +0900

    feat: 주문 조회 V5 : JPA 에서 DTO 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -69,4 +69,15 @@ public class OrderQueryRepository {
                         " join o.delivery d", OrderQueryDto.class
         ).getResultList();
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate,o.orderStatus, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
